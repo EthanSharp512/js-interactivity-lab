20efd894fc80475353c492320d2495e7011a3568
Ethan Sharp <ethansharpdev@gmail.com>
2022-04-27 16:30:13 -0600

    intermediate instructions step 1 solved

--- index.js
+++ index.js
@@ -1,10 +1,3 @@
-console.log('Hello world!')
-
-
-//In the crossOffMovie function, we’re going to do something similar, but we want to have different messages based off of whether the movie was just checked off as ‘watched’ or if it was added back to the list. So let’s start by creating the structure for an if/else block. Put it below where you toggled the checked class in the crossOffMovie function.
-
-
-
 const message = document.querySelector('#message');
 
 function addMovie(event) {
@@ -28,7 +21,8 @@ function addMovie(event) {
 
 function deleteMovie(event){
     event.target.parentNode.remove();
-    message.textContent = 'Movie deleted!'
+    message.textContent = 'Movie deleted!';
+    revealMessage();
 }
 
 const crossOffMovie = (event)  => {
@@ -38,9 +32,18 @@ const crossOffMovie = (event)  => {
     } else {
         message.textContent = "Movie added back"
     }
+
+    revealMessage();
 }
 
 const form = document.querySelector('form');
 form.addEventListener('submit', addMovie);
 
-//The condition of your if statement should check if it’s true that the event.target.classList contains the checked class. - contains is a built-in method that can be used on classList, the structure for doing so is event.target.classList.contains(‘some-class-name’)
+
+function revealMessage()  {
+    message.classList.remove('hide');
+    const fn = () => {message.classList.add('hide')}
+    setTimeout(fn, 1000)
+}
+
+//Create a function called revealMessage. Inside the function, call setTimeout, passing in a callback function and a time in milliseconds. - The callback function should add the hide class to message, you can see what the hide class does in the CSS file - We want the callback function to run 1 second after setTimeout is invoked, so for the second argument, pass in the number 1000
